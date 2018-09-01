0e09ceb83e39f2bb2d40f53376482805119bd68c
Fabio Soares Schmidt <fabio@improve.inf.br>
2018-09-01 19:17:52 -0300

    Update ajustar_flag_share.sh

--- ajustar_flag_share.sh
+++ ajustar_flag_share.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Fabio S. Schmidt - fabio@bktech.com.br
+#v1.0
 # *** SCRIPT VALIDO SOMENTE PARA AMBIENTES COM O ZIMLET SHARE TOOLKIT INSTALADO! ***
-#Script para remover a flag "i" caso encontre em alguma pasta 
+#Script para remover a flag "i" caso encontre em alguma pasta
 #Esse script ira recriar os compartilhamentos novamente
 #pastas com flag i podem nao ser exibidas pelos clientes de e-mail
 #
@@ -9,45 +10,57 @@
 #
 # - Script modificado para tratar acentuacao do portugues e traduzido
 
- 
-#Tratar acentuacao do portugues 
+#Tratar acentuacao do portugues
 export LC_ALL="pt_BR.UTF-8"
 account=$1
- 
+subzim="/usr/local/sbin/subzim"
+
 if [ x"$(id -n -u)" != x"zimbra" ]
 then
-	echo "Esse script deve ser executado com o usuario Zimbra"
-	exit 1
+        echo "Esse script deve ser executado com o usuario Zimbra"
+        exit 1
 fi
- 
+
 zmprov ga "$1" &> /dev/null
- 
+
 if [ $? -ne 0 ]
 then
-	echo "A conta informada nao existe"
-	exit 1
+        echo "A conta informada nao existe"
+        exit 1
 fi
- 
+
+echo "Atencao: Esse script e valido somente para ambientes com o share toolkit instalado"
+
+echo "Consultando os compartilhamentos existentes"
+shares=`zmmailbox -z -m $1 gfg / | awk '{print $3}' | grep -v -e '^Display' -e '-'`
+echo "Compartilhamentos existentes: $shares"
+
 zmmailbox -z -m "$account" gaf | sed "1,2d" | cut -d "/" -f "2-" | while read folder
 do
-	echo "Analisando a pasta '/$folder'"
-	oldflags=$(zmmailbox -z -m "$account" gf "/$folder" | grep flags | head -n 1 | cut -d ":" -f 2 | sed 's@[^"]*"\([^"]*\)".*@\1@')
-	echo " ** flags existentes : '$oldflags'"
-	newflags=$(echo "$oldflags" | sed 's@i@@g')
-	echo " ** flags aplicadas : '$newflags'"
- 
-	if [ x"$oldflags" != x"$newflags" ]
-	then
-		echo "=> Aplicando as novas flags - somente remove a flag i"
-		zmmailbox -z -m "$account" mff "/$folder" "$newflags"
-		if [ $? -eq 0 ]
-		then
-			echo "** Novas flags aplicadas com sucesso"
-		else
-			echo "** Falha ao aplicar as flags"
-		fi
-	else
-		echo "=> Nenhuma alteracao necessaria"
-	fi
-	echo "<-------------------------------------------->"
+        echo "Analisando a pasta '/$folder'"
+        oldflags=$(zmmailbox -z -m "$account" gf "/$folder" | grep flags | head -n 1 | cut -d ":" -f 2 | sed 's@[^"]*"\([^"]*\)".*@\1@')
+        echo " ** flags existentes : '$oldflags'"
+        newflags=$(echo "$oldflags" | sed 's@i@@g')
+        echo " ** novas flags : '$newflags'"
+
+        if [ x"$oldflags" != x"$newflags" ]
+        then
+                echo "=> Aplicando as novas flags - somente remove a flag i"
+                zmmailbox -z -m "$account" mff "/$folder" "$newflags"
+                if [ $? -eq 0 ]
+                then
+                        echo "** Novas flags aplicadas com sucesso"
+                else
+                        echo "** Falha ao aplicar as flags"
+                fi
+        else
+                echo "=> Nenhuma alteracao necessaria"
+        fi
+        echo "<-------------------------------------------->"
 done;
+
+echo "Aplicando novamente os compartilhamentos"
+      for SHARE in `echo $shares`
+do
+      $subzim $shares $1 2>/dev/null
+done
