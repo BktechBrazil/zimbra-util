e85aa97fc6d9275998a3bbb9deb0dc41e1445395
Fabio Soares Schmidt <fabio@improve.inf.br>
2021-05-25 18:06:09 -0300

    Update permissoes_run_cron.sh

--- enforce_sender/v2/permissoes_run_cron.sh
+++ enforce_sender/v2/permissoes_run_cron.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-/tmp/gerar_perms.sh
-/tmp/gerar_perms_listas.sh
+SCRIPT_DIR="/tmp"
+SENDER_ENFORCER_FILE="/opt/zimbra/conf/slm-exceptions-db"
 
-cat resultado_sendas.txt resultado_sendas_DistList.txt > /tmp/resultado.txt
+$SCRIPT_DIR/gerar_perms.sh
+$SCRIPT_DIR/gerar_perms_listas.sh
+
+#ARQUIVOS TEMPORARIOS SEMPRE SALVOS NO /TMP
+cat /tmp/resultado_sendas.txt /tmp/resultado_sendas_DistList.txt > $SENDER_ENFORCER_FILE
